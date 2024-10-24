e5adf14fa29f3248f1173a58cd3cbc5c0390d81f
ZeleZ <zelez-t@outlook.com>
2024-10-23 20:22:09 -0500

    feat: add database integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from '../internal/config/database.config';
+import { UsersModule } from '../internal/users/users.module';
+import { User } from '../internal/users/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [databaseConfig],
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('database.host'),
+                port: configService.get<number>('database.port'),
+                username: configService.get<string>('database.username'),
+                password: configService.get<string>('database.password'),
+                database: configService.get<string>('database.database'),
+                entities: [User],
+                synchronize: true,
+            }),
+            inject: [ConfigService],
+        }),
+        UsersModule,
+    ],
 })
 export class AppModule {}
--- src/internal/users/users.service.ts
+++ src/internal/users/users.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private repository: Repository<User>,
+    ) {}
+
+    create(user: User) {
+        this.repository.create(user);
+    }
+
+    async findAll(): Promise<User[]> {
+        return await this.repository.find();
+    }
+
+    async findOne(email: string): Promise<User> {
+        return await this.repository.findOneBy({ email: email });
+    }
+
+    async changePassword(email: string, newPassword: string) {
+        await this.repository.update(email, { password: newPassword });
+    }
+
+    async remove(email: string) {
+        await this.repository.delete({ email: email });
+    }
+}
